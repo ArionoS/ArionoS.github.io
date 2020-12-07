52a2f2a04182cf04273eae789c75ba5401f8dd35
ArionoS <arionoseptian0802@gmail.com>
2020-12-07 17:48:16 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+    <div class="footer">
+        <p>Copyright &copy Luxurious Hotel</p>
+    </div>
+    
+</body>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+    <div class="header">
+        <h4>Luxurious Hotel</h4>
+    </div>
+    <script src = "script.js"></script>
+</body>
